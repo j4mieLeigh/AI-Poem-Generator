d0c462984ed87d00b5b74ccb556d003a28f6e576
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-01-22 14:35:57 +0100

    Updated js and css to add pending message

--- src/script.js
+++ src/script.js
@@ -15,6 +15,8 @@ function generatePoem(event) {
   let prompt = `Provide a bad poem full of cliches, no more than 30 words long, about ${topic.value}. Separate each line of the poem with a <br>.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class = "blink">⌛ Generating a poem about ${topic.value}...</div>`;
   axios.get(apiUrl).then(displayPoem);
 }
 
