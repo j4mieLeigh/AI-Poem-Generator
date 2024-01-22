687fbdbe780b1284e66185dafcc0c39e51938a68
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-01-22 13:47:28 +0100

    Added API integration

--- src/script.js
+++ src/script.js
@@ -1,10 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings:
-      "You thought life <br />Couldn't get any worse <br />Didn't you? <br />Well, here's some poetry",
+    strings: response.data.answer,
     autoStart: true,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let topic = document.querySelector("#topic");
+  let apiKey = "5do93bc0b836c98t67cd86dfeaa4ff3f";
+  let context =
+    "You are an AI assistant knowledgeable about good poetry and bad poetry. You write only bad poetry full of cliches.";
+  let prompt = `Provide a bad poem full of cliches, no more than 30 words long, about ${topic.value}. Separate each line of the poem with a <br>.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let formElement = document.querySelector("#poem-generator-form");
