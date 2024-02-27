4644662a0a64afb3f8d14a65989771567d8cb90a
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-02-27 19:47:12 +0100

    Updated context

--- src/script.js
+++ src/script.js
@@ -11,7 +11,7 @@ function generatePoem(event) {
   let topic = document.querySelector("#topic");
   let apiKey = "5do93bc0b836c98t67cd86dfeaa4ff3f";
   let context =
-    "You are an AI assistant knowledgeable about good poetry and bad poetry. You write only bad poetry full of cliches.";
+    "You are an AI assistant knowledgeable about good poetry and bad poetry. You write only bad poetry full of cliches. You often use angsty words like midnight, darkness, despair, and sorrow.";
   let prompt = `Provide a bad poem full of cliches, no more than 30 words long, about ${topic.value}. Separate each line of the poem with a <br>.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
